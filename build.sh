d430ac68d6ca679cac6cc27afb990c43ea412e20
ItsTaconator <9751876+ItsTaconator@users.noreply.github.com>
2026-04-24 15:24:16 -0400

    Add building x64 Windows and AArch64 macOS binaries

--- build.sh
+++ build.sh
@@ -4,5 +4,9 @@ cargo clean
 cross build --target aarch64-unknown-linux-musl -r
 # Build for AArch64 GNU (example: Debian on a Pi)
 cross build --target aarch64-unknown-linux-gnu -r
+# Build for x64 Windows
+cross build --target x86_64-pc-windows-gnu
+# Build for AArch64 Mac
+cross build --target aarch64-apple-darwin
 # Build for native architecture
 cargo build --release
