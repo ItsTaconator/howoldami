1d2db16614147a533f66c93c05e08c7aabad82bb
ItsTaconator <9751876+ItsTaconator@users.noreply.github.com>
2026-04-24 15:27:49 -0400

    Fix macOS build support, and add messages to monitor progress

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 cargo clean
 # Build for AArch64 MUSL (example: Alpine Linux on a Pi)
+echo "Building for AArch64 Linux MUSL"
 cross build --target aarch64-unknown-linux-musl -r
 # Build for AArch64 GNU (example: Debian on a Pi)
+echo "Building for AArch64 Linux GNU"
 cross build --target aarch64-unknown-linux-gnu -r
 # Build for x64 Windows
+echo "Building for x64 Windows"
 cross build --target x86_64-pc-windows-gnu
-# Build for AArch64 Mac
-cross build --target aarch64-apple-darwin
+# Build for AArch64 Mac (needs aarch64-apple-darwin target installed)
+echo "Building for AArch64 Apple"
+cargo build --target aarch64-apple-darwin
 # Build for native architecture
 cargo build --release
